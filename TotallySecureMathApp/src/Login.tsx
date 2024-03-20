b7313f96c96ce091fd473ee38991326e4a1542ec
Ana Alarcon Figueroa <Ana.AlarconFigueroa@edu.sait.ca>
2024-03-20 15:39:43 -0600

    encrypted password

--- TotallySecureMathApp/src/Login.tsx
+++ TotallySecureMathApp/src/Login.tsx
@@ -1,87 +1,104 @@
-import React from 'react';
-import { View, Text, TextInput, Button, StyleSheet, Alert } from 'react-native';
-import type { NativeStackScreenProps } from '@react-navigation/native-stack';
-import { TRootStackParamList } from './App';
+import React from "react";
+import { View, Text, TextInput, Button, StyleSheet, Alert } from "react-native";
+import type { NativeStackScreenProps } from "@react-navigation/native-stack";
+import { TRootStackParamList } from "./App";
+/**
+ * Import bcryptjs library and create a salt variable
+ */
+var bcrypt = require("bcryptjs");
+var salt = bcrypt.genSaltSync(10);
 
 export interface IUser {
-	username: string;
-	password: string;
+  username: string;
+  password: string;
 }
 
 interface IProps {
-	onLogin: (user: IUser) => void;
+  onLogin: (user: IUser) => void;
 }
 
-type TProps = NativeStackScreenProps<TRootStackParamList, 'Login'> & IProps;
+type TProps = NativeStackScreenProps<TRootStackParamList, "Login"> & IProps;
 
 export default function Login(props: TProps) {
-	const [username, setUsername] = React.useState('');
-	const [password, setPassword] = React.useState('');
+  const [username, setUsername] = React.useState("");
+  const [password, setPassword] = React.useState("");
 
-	const users: IUser[] = [
-		{ username: 'joe', password: 'secret' },
-		{ username: 'bob', password: 'password' },
-	];
+  /*
+   * Array of users with dynamically hashed passwords adding salt
+   */
+  const users: IUser[] = [
+    { username: "joe", password: bcrypt.hashSync("secret", salt) },
+    { username: "bob", password: bcrypt.hashSync("password", salt) },
+  ];
 
-	function login() {
-		let foundUser: IUser | false = false;
+  function login() {
+    let foundUser: IUser | false = false;
 
-		for (const user of users) {
-			if (username === user.username && password === user.password) {
-				foundUser = user;
+    for (const user of users) {
+      if (username === user.username) {
+        /*
+         * Compare hashed password with entered password
+         */
+        const isPasswordMatch = bcrypt.compare(password, user.password);
+        if (isPasswordMatch) {
+          foundUser = user;
+          break;
+        }
+      }
+    }
 
-				break;
-			}
-		}
+    if (foundUser) {
+      props.onLogin(foundUser);
+    } else {
+      Alert.alert("Error", "Username or password is invalid.");
+    }
+  }
 
-		if (foundUser) {
-			props.onLogin(foundUser);
-		} else {
-			Alert.alert('Error', 'Username or password is invalid.');
-		}
-	}
-
-	return (
-		<View style={styles.container}>
-			<Text style={styles.title}>Login</Text>
-			<TextInput
-				style={styles.username}
-				value={username}
-				onChangeText={setUsername}
-				placeholder="Username"
-			/>
-			<TextInput
-				style={styles.password}
-				value={password}
-				onChangeText={setPassword}
-				placeholder="Password"
-			/>
-			<Button title="Login" onPress={login} />
-		</View>
-	);
-};
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Login</Text>
+      <TextInput
+        style={styles.username}
+        value={username}
+        onChangeText={setUsername}
+        placeholder="Username"
+      />
+      <TextInput
+        style={styles.password}
+        value={password}
+        onChangeText={setPassword}
+        placeholder="Password"
+        /**
+         * Set secureTextEntry to true to hide the password
+         */
+        secureTextEntry={true}
+      />
+      <Button title="Login" onPress={login} />
+    </View>
+  );
+}
 
 const styles = StyleSheet.create({
-	container: {
-		flex: 1,
-		padding: 20,
-		backgroundColor: '#fff',
-	},
-	title: {
-		fontSize: 24,
-		fontWeight: 'bold',
-		marginBottom: 20,
-	},
-	username: {
-		borderWidth: 1,
-		borderColor: '#ccc',
-		padding: 10,
-		marginBottom: 10,
-	},
-	password: {
-		borderWidth: 1,
-		borderColor: '#ccc',
-		padding: 10,
-		marginBottom: 10,
-	}
+  container: {
+    flex: 1,
+    padding: 20,
+    backgroundColor: "#fff",
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: "bold",
+    marginBottom: 20,
+  },
+  username: {
+    borderWidth: 1,
+    borderColor: "#ccc",
+    padding: 10,
+    marginBottom: 10,
+  },
+  password: {
+    borderWidth: 1,
+    borderColor: "#ccc",
+    padding: 10,
+    marginBottom: 10,
+  },
 });
